8020cd8e89981bd922724f66013839a378de30a7
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-11-26 19:29:30 +0800

    Add files via upload

---
+++ tuple&list.py
@@ -0,0 +1,19 @@
+# M Naufal al varzy
+#D0425514
+
+#tuple
+
+Warna = ("merah", "kuning", "hijau") #tuple tidak bisa di ubah
+print("isi tuple: ", Warna)
+print("Elemen ke-1:", Warna[0])
+# warna[1] = "kuning"  # Ini akan ERROR karena tuple tidak bisa diubah
+
+
+#List
+Buah = ["semangka", "apel", "jeruk"] #Bisa diubah
+print("Sebelum diubah:", Buah)
+
+Buah[1] = "jeruk"     # mengubah isi
+Buah.append("melon")  # menambah data
+
+print("Setelah diubah:", Buah)
